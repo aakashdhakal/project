2bda4b25f9c0ee2ad26c4d13691fb9a994b8ddc9
aakashdhakal <53038685+aakashdhakal@users.noreply.github.com>
2020-05-29 14:17:41 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -21,3 +21,49 @@ $(document).on('click', '.toggle-password', function() {
     var input = $("#psw");
     input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
 });
+ toggleswitch = document.getElementById('#theme-toggler')
+$(document).ready(function(){
+
+    // Check local storage and set theme
+    if(localStorage.theme) {
+
+        $('body').addClass( localStorage.theme );
+
+    } 
+    else{
+
+        $('body').addClass('light-mode'); // set default theme. No need to set via php now
+
+    }
+
+
+
+    //Switch theme and store in local storage ...
+
+    $("#theme-toggler").click(function(){
+
+         if ($('body').hasClass( 'light-mode')){
+
+            $('body').removeClass('light-mode').addClass('dark-mode');
+            localStorage.theme = 'dark-mode';
+
+
+         }
+         else  {
+
+            $('body').removeClass('dark-mode').addClass('light-mode');
+            localStorage.theme = 'light-mode';
+
+         }
+       });
+     });
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark-mode') 
+	{
+        toggleSwitch.checked = true;
+    }
+}
