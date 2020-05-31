ad52435d56b8058690f032bfd1c8810072be37ee
aakashdhakal <53038685+aakashdhakal@users.noreply.github.com>
2020-05-31 12:13:14 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop < 20) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
-    mybutton.style.display = "block";
+    mybutton.style.display = "none";
   }
 }
 function topFunction() {
@@ -67,4 +67,12 @@ if (currentTheme) {
 	{
         toggleSwitch.checked = true;
     }
+}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
